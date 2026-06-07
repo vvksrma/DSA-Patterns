981689358684be23f47c54a6199d21479630e858
vvksrma <viveksharma0390@gmail.com>
2026-06-07 12:38:00 +0530

    code

--- Two pointers/Pair with Target sum.cpp
+++ Two pointers/Pair with Target sum.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 
 int main(){
+    int arr[] = {2, 5, 1, 3, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 5;
 
+    sort(arr, arr + n);
+
+    int i = 0, j = n - 1;
+
+    while(i < j){
+        int sum = arr[i] + arr[j];
+        if(sum == target){
+            cout << arr[i] << " " << arr[j] << '\n';
+            break;
+        } else if(sum < target){
+            i++;
+        } else {
+            j--;
+        }
+    }
     return 0;
 }
